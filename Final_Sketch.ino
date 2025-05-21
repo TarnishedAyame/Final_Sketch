18d6c51f3e5ea1c81c9b25726e3098766c442357
TarnishedAyame <130957977+TarnishedAyame@users.noreply.github.com>
2025-05-21 02:53:25 -0400

    Final Assignment

--- Final_Sketch.ino
+++ Final_Sketch.ino
@@ -7,7 +7,7 @@ const int ledPin2 = 15;
 int leftButtonPin = 9; //Left Input
 int rightButtonPin= 10;//Right Input
 
-const int servoPin = 42;
+const int potPin = 2;
 const int adcPin = 1;
 
 int value;
@@ -26,19 +26,30 @@ void setup() {
 }
 
 void loop() {
+  // Read the Pots Value
+  int potValue = analogRead(potPin);
+  
+  //maps the pot value to the LED brightness
+  int brightness = map(potValue, 0, 1023, 0, 255);
+
+  //LED's brightness is set using PWM
+  analogWrite(ledPin, brightness);
+  Serial.print("Brightness");
+  Serial.println(brightness);
   // put your main code here, to run repeatedly:
   //Led Pins to show that the buttons work or which diretion it's going
-  if(digitalRead(leftButtonPin) == LOW) {
-    digitalWrite(ledPin, HIGH);
+ /* if(digitalRead(leftButtonPin) == LOW) {
+    digitalWrite(ledPin, LOW);
   }
   else{ // if button is pressed
-  digitalWrite(ledPin,LOW);
-  }
+  digitalWrite(ledPin,HIGH);
+  }*/
   //Right Button 
   if(digitalRead(rightButtonPin) == LOW) {
-    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin2, LOW);
   }
   else{ // if button is pressed
-  digitalWrite(ledPin2,LOW);
+  digitalWrite(ledPin2,HIGH);
+
   }
 }
